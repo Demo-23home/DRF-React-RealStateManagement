c6ff9bb7074c833f6ce4d1fbc2d0c72b9b150dd2
Demo-23home <zeyadsalama23@gmail.com>
2025-01-30 20:41:21 +0200

    Adding (Home, Notfound) pages

--- client/src/lib/fonts.ts
+++ client/src/lib/fonts.ts
@@ -2,11 +2,13 @@
 import { Open_Sans, Roboto_Slab } from "next/font/google";
 
 export const openSans = Open_Sans({
-    subsets: ["latin"], 
-    variable: "--font-openSans"
-})
+	subsets: ["latin"],
+	weight: ["400", "600"], 
+	variable: "--font-openSans",
+});
 
 export const robotoSlab = Roboto_Slab({
-    subsets: ["latin"], 
-    variable: "--font-robotoSlab"
-})
+	subsets: ["latin"],
+	weight: ["400", "600"], 
+	variable: "--font-robotoSlab",
+});
